b6cbb63d29ce6a9472122db3bbead1d6cd8c49cb
andrey.voropay <avoropay@ukr.net>
2017-01-26 09:44:04 +0200

    init

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -14,6 +14,37 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        $client = new \Zend\Http\Client();
+        $client->setUri('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+        $response = $client->send();
+
+        $page = (int) $this->params()->fromQuery('page', 1);        
+        if ($response->isSuccess()) {
+            $rates = json_decode($response->getBody());
+            $currency_paginator = new \Zend\Paginator\Paginator(new \Zend\Paginator\Adapter\ArrayAdapter($rates));
+            $page = ($page < 1) ? 1 : $page;
+            $currency_paginator->setCurrentPageNumber($page);
+
+            // Set the number of items per page to 10:
+            $currency_paginator->setItemCountPerPage(10);
+        }
+
+        $client->setUri('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+        $response = $client->send();
+        if ($response->isSuccess()) {
+            $privat = json_decode($response->getBody());
+        }
+
+        $client->setUri('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11');
+        $response = $client->send();
+        if ($response->isSuccess()) {
+            $privat1 = json_decode($response->getBody());
+        }
+
+        return new ViewModel(['paginator' => $currency_paginator,
+                            'privat' => $privat,
+                            'privat1' => $privat1
+            ]
+        );
     }
 }
